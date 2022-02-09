2e9e70d08df74b9ee20d1c014b1589cd2c42d16c
Robert Sainton <robertsainton@outlook.com>
2022-02-09 18:58:26 -0500

    commit tree 7a99254babe66159eaabbaeeaabc670b95cb3b8e

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(somenumber){
   // Write your code here!
+      if (somenumber <= '400') { return 'This one is on me!'}
+    else if (somenumber <= '2500') {return 'I will gladly take your thirty bucks.'}
+    else if (somenumber > '2500') {return 'No can do.'}
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(cityname){
   // Write your code here!
+      return cityname === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(typeoftype){
   // Write your code here!
+  switch (typeoftype) {
+    case 'generous': 
+          return 'Thank you so much.'
+    case 'not as generous': 
+          return 'Thank you.'
+    default: 
+          return 'Bye.'
+  }
 }
